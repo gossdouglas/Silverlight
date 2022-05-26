1db9e022298d6081ed6141dede739e9c023a92b7
gossdouglas <gossdouglas@yahoo.com>
2022-05-26 14:53:49 -0500

    commit.

---
+++ OilCanningSilverLightToHtml5Js/OilCanningSilverLightToHtml5Js/bin/Debug/Output/OilCanningSilverLightToHtml5Js.meta.js
@@ -0,0 +1,12 @@
+Bridge.assembly("OilCanningSilverLightToHtml5Js", function ($asm, globals) {
+    "use strict";
+
+
+    var $m = Bridge.setMetadata,
+        $n = ["System","System.Windows.Controls"];
+    $m("ǀǀOilcanningsilverlighttohtml5JsǀǀComponentǀǀAppǀǀXamlǀǀFactory", function () { return {"att":1048961,"a":2,"s":true,"m":[{"a":2,"n":"Instantiate","is":true,"t":8,"sn":"Instantiate","rt":$n[0].Object}]}; }, $n);
+    $m("ǀǀOilcanningsilverlighttohtml5JsǀǀComponentǀǀMainpageǀǀXamlǀǀFactory", function () { return {"att":1048961,"a":2,"s":true,"m":[{"a":2,"n":"Instantiate","is":true,"t":8,"sn":"Instantiate","rt":$n[0].Object}]}; }, $n);
+    $m("OilCanningSilverLightToHtml5Js.App", function () { return {"att":1048833,"a":2,"m":[{"a":2,"n":".ctor","t":1,"sn":"ctor"},{"a":2,"n":"InitializeComponent","t":8,"sn":"InitializeComponent","rt":$n[0].Void},{"a":2,"n":"Main","is":true,"t":8,"sn":"Main","rt":$n[0].Void},{"a":1,"n":"_contentLoaded","t":4,"rt":$n[0].Boolean,"sn":"_contentLoaded","box":function ($v) { return Bridge.box($v, System.Boolean, System.Boolean.toString);}}]}; }, $n);
+    $m("OilCanningSilverLightToHtml5Js.MainPage", function () { return {"att":1048577,"a":2,"m":[{"a":2,"n":".ctor","t":1,"sn":"ctor"},{"a":2,"n":"InitializeComponent","t":8,"sn":"InitializeComponent","rt":$n[0].Void},{"a":4,"n":"TextBlock1","t":4,"rt":$n[1].TextBlock,"sn":"TextBlock1"},{"a":1,"n":"_contentLoaded","t":4,"rt":$n[0].Boolean,"sn":"_contentLoaded","box":function ($v) { return Bridge.box($v, System.Boolean, System.Boolean.toString);}}]}; }, $n);
+    $asm.attr= [new CSHTML5.Internal.Attributes.CompilerVersionNumberAttribute("2.0.0.490668"),new CSHTML5.Internal.Attributes.CompilerVersionFriendlyNameAttribute("Version 2.0 Preview 0.6 (2.0.0-alpha49-068) (2019.10.06)"),new CSHTML5.Internal.Attributes.CompilerIsSLMigrationAttribute(true),new CSHTML5.Internal.Attributes.IntermediateOutputAbsolutePathAttribute("C:\\Users\\gosstony\\source\\repos\\Silverlight\\Silverlight\\OilCanningSilverLightToHtml5Js\\OilCanningSilverLightToHtml5Js\\obj\\Debug\\"),new CSHTML5.Internal.Attributes.SettingsAttribute()];
+});
